67c77a79d6ad5c153e2eb90da99d61c6c49d52d7
Luiz Tanure <letanure@gmail.com>
2015-03-16 15:36:13 -0300

    add deploy github pages

---
+++ .gulp/_deploy.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var gulp   = require('gulp');
+var $      = require('gulp-load-plugins')();
+var config = require('./appConfig');
+var helpers= require('./helpers');
+
+gulp.task('deploy-ghp', function() {
+  return gulp.src(config.path.dist.root + '**/*')
+    .pipe($.ghPages());
+});
