f0018db97440bd84ca57808e368cbd85cfe5259c
Luiz Tanure <letanure@gmail.com>
2015-04-08 19:11:20 -0300

    add gulp js hint task

---
+++ .gulp/_lint.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var gulp         = require('gulp');
+var $            = require('gulp-load-plugins')();
+$.mainBowerFiles = require('main-bower-files');
+var config       = require('./appConfig');
+var helpers      = require('./helpers');
+var stylish      = require('jshint-stylish');
+
+var map = require('map-stream');
+var events = require('events');
+var notify = require('gulp-notify');
+var emmitter = new events.EventEmitter();
+var path = require('path');
+
+var jsHintErrorReporter = map(function (file, cb) {
+  if (!file.jshint.success) {
+    file.jshint.results.forEach(function (err) {
+      if (err) {
+        var msg = [
+          path.basename(file.path),
+          'Line: ' + err.error.line,
+          'Reason: ' + err.error.reason
+        ];
+        emmitter.emit('error', new Error(msg.join('\n')));
+      }
+    });
+  }
+  cb(null, file);
+});
+
+gulp.task('lint-scripts', function() {
+  gulp.src(config.path.src.scripts)
+    .pipe($.plumber())
+    .pipe($.jshint('.jshintrc', {fail: true}))
+    .pipe($.jshint.reporter(stylish))
+    .pipe(jsHintErrorReporter)
+    .on('error', $.notify.onError(function (error) {
+      return error.message;
+    }));
+
+});
