be6491eed0f28c7dd8134bb6600811599a2a1673
Luiz Tanure <letanure@gmail.com>
2015-04-10 13:39:13 -0300

    add task speed

--- .gulp/_utils.js
+++ .gulp/_utils.js
@@ -3,8 +3,22 @@
 var gulp   = require('gulp');
 var $      = require('gulp-load-plugins')();
 var config = require('./appConfig');
+var psi    = require('psi');
 
 gulp.task('clear', function() {
   return gulp.src(config.path.dist.root, { read: false })
     .pipe($.rimraf());
+});
+
+
+gulp.task('speed', function() {
+  psi.output('https://oioi.firebaseapp.com/produtos-e-servicos/',{
+    // key: '',
+    strategy: 'mobile', // desktop || mobile
+    threshold: 100
+
+  }, function (err, data) {
+    console.log('done');
+
+  });
 });
