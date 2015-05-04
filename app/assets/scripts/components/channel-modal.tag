bdc3a39a4756d12f6d1a0ac33a34e4064e1667cf
Luiz Tanure <letanure@gmail.com>
2015-05-04 06:03:51 -0300

    channel modal

---
+++ app/assets/scripts/components/channel-modal.tag
@@ -0,0 +1,51 @@
+<channel-modal>
+  <div role="dialog" class="channel-modal { data.klass }">
+    <div class="channel-modal_image" style="background-image:url(/assets/images/demo/bg-warner.jpg)"></div>
+    <div class="channel-modal_indisponible">
+      <div>
+        Indisponível neste plano
+      </div>
+    </div>
+    <div class="channel-modal_close">Fechar</div>
+    <div class="channel-modal_content">
+      <div class="channel-modal_logo" style="background-image:url(/assets/images/logos/prov/byu-tv.png);"></div>
+      <div class="channel-modal_name">{ data.name }</div>
+      <div class="channel-modal_channel">Canal 64</div>
+      <div class="channel-modal_info">
+        <p>Warner Channel é um canal de televisão por assinatura que é transmitido na América Latina e no Sudeste da Ásia pertencente a HBO (em conjunto com a empresa-irmã Warner Bros).</p>
+        <p>A maior parte de sua programação tem o áudio original em Inglês com legendas em Português (para o Brasil), possuindo também poucos programas dublados.</p>
+        <p>Os seriados exibidos no canal são principalmente da The CW, FOX, ABC, NBC e CBS. O acervo de filmes é da Warner Bros, e reúne as principais séries das produtoras Warner Bros. Television e da CBS Television Studios dos Estados Unidos. </p>
+      </div>
+      <div class="channel-modal_disponible">
+        <div class="channel-modal_disponible_title">Disponível nos planos</div>
+        <div class="channel-modal_disponible_list">
+          <ul>
+            <li>
+              <a href="#">TV Start HD</a> - Plano à partir de R$ 59,90
+              <a href="#">TV Mix HD</a> - Plano à partir de R$ 79,90
+              <a href="#">TV Total HD</a> - Plano à partir de R$ 9  9,90
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="channel-modal_action">
+        <div class="channel-modal_action_title">Já é cliente Oi TV HD?</div>
+        <div class="channel-modal_action_link">
+          <a href="">Faça um Upgrade do seu plano</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script>
+    var self = this;
+    self.data = {}
+    loadChannel(urlApi){
+      console.log('lek lek', urlApi);
+      $.getJSON(urlApi, function(json){
+        console.log(json);
+        self.data = json.data;
+        self.update()
+      });
+    }
+  </script>
+</channel-modal>
--- app/assets/scripts/pages/produtos-e-servicos.js
+++ app/assets/scripts/pages/produtos-e-servicos.js
@@ -1,60 +1,102 @@
 define([
-  'core/dom',
-  'vendor/reqwest',
+  'domlib',
   'vendor/lodash',
   'vendor/riot',
-  'vendor/velocity',
-  'channel-addons',
-  'tags/card',
-  'tags/price',
-  ],function (dom, reqwest, lodash, riot, velocity, addOns) {
+  'velocity',
+  // 'velocity-ui',
+  'ScrollMagic',
+  'tags/channel-modal',
+  ],function ($, lodash, riot, Velocity, ScrollMagic, channelModal) {
+
 
 
   var _public = {},
-      _private = {},
-      $body = $('body')[0],
-      $container = $('.container')[0],
-      channelsPrices;
+      _private = {};
+      $openClientType    = $('.oi-channels_header_client-type-trigger'),
+      $dropdowClientType = $('.oi-channels_header_client-type_dropdow'),
+      $clientType        = $dropdowClientType.find('a'),
+      $channelsContainer = $('.oi-channels-lists_list-tv_container'),
+      $channelLink       = $channelsContainer.find('a')
+
+  var config = {
+    api: {
+      channel: '/api/channel/'
+    }
+  }
 
 
   _public.init = function(){
-    // _private.bindOpenModal();
-    // _private.bindCloseButton();
+    _private.openClientType();
+    _private.changeClientType();
+    _private.bindOpenModalChannel();
+    _private.bindCloseButton();
     // _private.bindNavKeyboard();
     // addOns.init()
-    _private.loadPrice();
+    // _private.loadPrice();
   };
 
-  _private.loadPrice = function(){
-    return
-    reqwest('/api/price/rj.json', function (resp) {
-      channelsPrices = resp.data;
-
-      _.find(channelsPrices, function(channelPrices){
-        return channelPrices.default;
-      });
-
-       riot.mount('oi-card', { greeting: 'Hola', punctuation: "?"})
-       riot.mount('oi-price')
-
-
-
+  _private.openClientType = function(){
+    $openClientType.on('click', function(evt){
+      evt.preventDefault();
+      $dropdowClientType.toggleClass('open');
     });
-
   }
 
-  // _private.bindOpenModal = function(){
-  //   $('.channels-pack a').on('click', function(e){
-  //     e.preventDefault();
-  //     _private.openModal();
-  //   });
-  // };
+  _private.changeClientType = function(){
+    $clientType.on('click', function(evt){
+      evt.preventDefault();
+      $clientType.removeClass('active');
+      $(this).addClass('active')
+      // setTimeout( function () {
+        $dropdowClientType.removeClass('open');
+       // }, 500);
+      // @mock
+      var $bt = $(this)
+      var price = $bt.find('.oi-price_value_integer').text()
+      $('.oi-channels_header_price .oi-price_value_integer').text(price)
+    });
+  }
+  // _private.loadPrice = function(){
+  //   return
+  //   reqwest('/api/price/rj.json', function (resp) {
+  //     channelsPrices = resp.data;
 
-  // _private.bindCloseButton = function(){
-  //   $('.modal-close').on('click', function(e){
-  //     _private.closeModal();
+  //     _.find(channelsPrices, function(channelPrices){
+  //       return channelPrices.default;
+  //     });
+
+  //      riot.mount('oi-card', { greeting: 'Hola', punctuation: "?"})
+  //      riot.mount('oi-price')
   //   });
-  // };
+  // }
+
+  _private.bindOpenModalChannel = function(){
+    $channelLink.on('click', function(e){
+      e.preventDefault();
+      // riot.observable(this)
+
+      $channelsContainer.find('a.active').removeClass('active')
+      $(this).addClass('active')
+      var urlPage = $(this)[0].href
+      _private.fillDetail(urlPage);
+    });
+  };
+
+  var cmodal = riot.mount('channel-modal')[0]
+
+  _private.fillDetail = function(urlPage){
+    var urlApi = config.api.channel + urlPage.split('/').slice(-1)[0] + '.json';
+    $('.channel-modal').show()
+    cmodal.loadChannel(urlApi)
+    _private.bindCloseButton()
+  };
+
+  _private.bindCloseButton = function(){
+    $('.channel-modal_close').on('click', function(e){
+      console.log('close');
+      _private.closeModal();
+    });
+  };
 
   // _private.bindNavKeyboard = function(){
   //   document.onkeydown = function(evt) {
@@ -74,17 +116,14 @@ define([
   //   };
   // };
 
-  // _private.openModal = function(){
-  //   $('.modal')[0].style.display = 'block';
-  //   $container.attributes['aria-hidden'].value = true;
-  //   // $body.addClass('modal-opened');
+  _private.openModal = function(){
+    console.log('open');
+    // $body.addClass('modal-opened');
+  };
 
-  // };
-
-  // _private.closeModal = function(){
-  //   $('.modal')[0].style.display = 'none';
-  //   $container.attributes['aria-hidden'].value = false;
-  // };
+  _private.closeModal = function(){
+    $('.channel-modal').hide()
+  };
 
   return _public;
 
