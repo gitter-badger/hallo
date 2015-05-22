d36554e3ed4465d6df2c4d0dce954c7e29e9dc2a
Luiz Tanure <letanure@gmail.com>
2015-05-22 18:28:23 -0300

    fixo table compare

--- app/assets/scripts/pages/fixo/main.js
+++ app/assets/scripts/pages/fixo/main.js
@@ -1,13 +1,29 @@
 // main.js
 define([
-  ], function(){
+  'domlib',
+  'vendor/lodash',
+  'vendor/riot',
+  'tags/oi-price',
+  ], function($, _, riot, tagPrice){
 
   var _private = {};
   var _public = {};
 
   _public.init = function(){
     console.log('fixo, _public.init');
+    _private.loadPlans()
   };
+
+  _private.loadPlans = function (){
+    _private.showPrices()
+  }
+  _private.showPrices = function (){
+    riot.mount('#price-fixo-regular', { price: 59.90, small: true });
+    riot.mount('#price-fixo-ddd',     { price: 49.90, small: true });
+  }
+
+  // oiMediator.publish( 'start plan', defaultPlan );
+  // oiMediator.subscribe( 'addon add', _public.addAddon );
 
   return _public;
 });
